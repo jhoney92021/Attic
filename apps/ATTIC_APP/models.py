1e8c317fd0a9ea7ccfe1eaf98be02c5b1274e1ab
jhoney92021 <51093828+jhoney92021@users.noreply.github.com>
2019-07-22 15:39:02 -0700

    No more 1-1, companies working with pages

--- apps/ATTIC_APP/models.py
+++ apps/ATTIC_APP/models.py
@@ -13,6 +13,7 @@ from apps.LOGIN_APP.models import Users
 class Junk(models.Model):#ALSO REFFERED TO AS JUNK
     name = models.CharField(max_length=20)#NAME OF JUNK
     description = models.TextField()#DESCRIPTION OF JUNK
+    location = models.CharField(max_length=20, default='None added yet')#LOCATION OF JUNK
     poster = models.ForeignKey(Users, related_name='posted')#PERSON WHO OWNS JUNK
     holder = models.ForeignKey(Users, related_name='holding')#PERSON IN POSSESSION  OF THE JUNK, OWNER, OR RENTER ETC 
     created_at = models.DateTimeField(auto_now_add=True)
@@ -21,7 +22,7 @@ class Junk(models.Model):#ALSO REFFERED TO AS JUNK
 class Family(models.Model):#FOR TAGGING AN ITEM AS 'AUTOMOTIVE', OR 'KITCHEN
     name = models.CharField(max_length=20)#NAME OF FAMILY
     description = models.TextField()#DESCRIPTION OF FAMILY
-    equipment = models.ManyToManyField(Junk, related_name='family')
+    junk = models.ManyToManyField(Junk, related_name='family')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -29,7 +30,7 @@ class Review(models.Model):#EVERYTHING CAN BE REVIEWED
     content = models.TextField(max_length = 180) 
     rating = models.IntegerField()
     creator = models.ForeignKey(Users, related_name='created_review') 
-    equipment = models.ManyToManyField(Junk, related_name='reviewed')
+    junk = models.ManyToManyField(Junk, related_name='reviewed')
     user = models.ManyToManyField(Users, related_name='reviewed')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
